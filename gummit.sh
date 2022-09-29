3e84e73b858cee11405ab97ff1802626504a5a66
petereon <pvyboch1@gmail.com>
2022-09-29 19:11:03 +0200

    feat: adding optinal unstaging

--- gummit.sh
+++ gummit.sh
@@ -2,9 +2,16 @@
 
 exit_if_130 (){
     if  [ $? -eq 130 ]; then
+        eval $1
         echo "​❌​ SIGINT caught! Exiting!" 
         exit 1
     fi
+}
+
+reset_if_was_added (){
+    if [[ -v$RESET_COMMAND ]]; then
+        gum confirm "Unstage files?" && echo "Unstaging staged files!" && eval $RESET_COMMAND > /dev/null
+    fi 
 }
 
 if [[ "$*" == *"--add"* || "$*" == *"-"*"a"* || "$#" -eq 0 ]]; then
@@ -22,6 +29,7 @@ if [[ "$*" == *"--add"* || "$*" == *"-"*"a"* || "$#" -eq 0 ]]; then
         files+="$(printf '%s' $file | rev | cut -d ' ' -f1 | rev | tr '\n' ' ' )"
     done
     ADD_COMMAND="git add $files"
+    RESET_COMMAND="git reset $files"
     echo Executed: "$(echo $ADD_COMMAND | lolcat)"
     eval $ADD_COMMAND
 fi
@@ -30,20 +38,20 @@ FAILED=0
 if [[ "$*" == *"--commit"* || "$*" == *"-"*"c"* || "$#" -eq 0 ]]; then
     echo "\n" 🔧 Creating an $(echo "(un)conventional" | lolcat)commit! "\n"
 
-    TYPE=$(gum choose "fix" "feat" "docs" "style" "refactor" "test" "chore" "revert")
-    exit_if_130
+    TYPE=$(gum choose "fix" "feat" "docs" "test" "style" "refactor" "build" "ci" "perf" "revert")
+    exit_if_130 reset_if_was_added
     SCOPE=$(gum input --placeholder "scope")
-    exit_if_130
+    exit_if_130 reset_if_was_added
     # Since the scope is optional, wrap it in parentheses if it has a value.
     test -n "$SCOPE" && SCOPE="($SCOPE)"
 
     # Pre-populate the input with the type(scope): so that the user may change it
     SUMMARY=$(gum input --value "$TYPE$SCOPE: " --placeholder "Summary of this change")
-    exit_if_130
+    exit_if_130 reset_if_was_added
 
     DESCRIPTION=""
     gum confirm "Add long description?" && DESCRIPTION=$(gum write --placeholder "Details of this change")
-    exit_if_130
+    exit_if_130 reset_if_was_added
 
     COMMIT_COMMAND="git commit -m \"$SUMMARY\" -m \"$DESCRIPTION\""
 
